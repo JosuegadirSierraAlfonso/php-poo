ebcfe3407f45317bd7e3486cfd93d2aab83ab8e0
JosuegadirSierraAlfonso <josuegsierraa18@gmail.com>
2023-05-17 13:33:42 -0500

    feat: :memo: Exercise Person
    A exercise with a class Person taht add name, last name and age

--- api.php
+++ api.php
@@ -1,53 +1,87 @@
 <?php
-    // class olla{
-    //     public $lugares;
-    //     private $merca;
-    //     public function __construct(){
+    /* class olla{
+        public $lugares;
+        private $merca;
+        public function __construct(){
+        }
+        public function set_lugar():void {
+            $arg = (object) func_get_arg();
+            $this->lugares = $p1;
+        }
+        public function get_lugar():string {
 
-    //     }
-    //     public function set_lugar():void {
-    //         $arg = (object) func_get_arg();
-    //         $this->lugares = $p1;
-    //     }
-    //     public function get_lugar():string {
-            
-    //         return $this->lugares;
-    //     }
-    //     // public function __destruct(){
-    //     //     echo "Se allanado la ".__CLASS__;
-    //     // }
-    // }
-  
+            return $this->lugares;
+        }
+        public function __destruct(){
+            echo "Se allanado la ".__CLASS__;
+        }
+    } */
+
+
+
+
+
     /* function data(){
         var_dump(func_get_arg(3));
     }
 
-    data("miguel",23,true,["A","B"]);*/
-
-
-    /* function cursos(){
-        var_dump (func_get_arg(3));
-    }
-    cursos("josue",23,"tasa",["B","C"]); */
-
-
-     /* $week = array('juan','33','julia','76');
-     $position = count($week) -1;
-     echo $week[$position];  */
+    data("miguel",23,true,["A","B"]); */
 
 
 
-    class person{
+
+
+/*     class Person{
         public $name;
         public $last_name;
         public $age;
 
         function __construct($name, $last_name, $age){
-            $this -> name = $name;
-            $this -> last_name = $last_name;
-            $this -> age = $age;
+            $this->name = $name;
+            $this->last_name = $last_name;
+            $this->age = $age;
         }
 
+        function get($properties){
+            if(property_exists($this, $properties)){
+                return $this->$properties;
+            }   
+        }
+        function set($properties, $valor){
+            if(property_exists($this, $properties)){
+                $this->$properties = $valor;
+            }
+        }
+        function mayor_age(){
+            return $this->age >= 18;
+        }
+        function agge(){
+            return $this->age;
+        }
+        function full_name(){
+            return $this->name." ".$this->last_name;
+        }
     }
+$person = new Person("gadir", "sierra", 8);
+
+if($person->mayor_age()){
+    echo $person->full_name(). " mayor"." ".$person->agge();
+}else{
+    echo $person->full_name()." menor"." ".$person->agge();
+}
+echo $person->name1;
+ */
+
+
+
+
+/*  */
+
+
+
+
+
+
+
     /* https://www.discoduroderoer.es/ejercicios-propuestos-y-resueltos-poo-php/ */
 ?>
